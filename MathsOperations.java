39a4b7b397534f878a82ce586e55702e355f29ac
amidh <amidp18@gmail.com>
2021-10-23 19:54:07 +0100

    first

---
+++ MathsOperations.java
@@ -0,0 +1,13 @@
+public class MathsOperations {
+    public void a1(int a, int b) {
+        System.out.println(a-b);
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+    public static void main(String[] ami ) {
+        MathsOperations mathsOperations = new MathsOperations();
+        mathsOperations.a1(25,30);
+    }
+}
+
